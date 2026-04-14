a332ae3dfaab90ac62d8ccd89684f1348700a48c
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-04-14 21:56:50 +0300

    Implement CRUD operations in main.ts

---
+++ TypeScript/task_3/js/main.ts
@@ -0,0 +1,25 @@
+// task_3/js/main.ts
+
+/// <reference path="crud.d.ts" />
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva"
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+console.log(`Inserted row ID: ${newRowID}`);
+
+const updatedRow: RowElement = { 
+  firstName: "Guillaume", 
+  lastName: "Salva", 
+  age: 23 
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+console.log(`Updated row: ${newRowID}`, updatedRow);
+
+CRUD.deleteRow(newRowID);
+console.log(`Deleted row ID: ${newRowID}`);
